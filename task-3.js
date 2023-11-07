a0b1ebe979569a16378169e9cc53588657d9689a
zhornyk-maria <zhornyk.maria@gmail.com>
2023-11-07 14:35:32 +0200

    homework1 done

---
+++ task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    console.log(`${contentWidth + paddingWidth * 2 + borderWidth * 2}`);
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
